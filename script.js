d806a59a578e0e9a9370c93023858fad27791ed9
JwhCK <jacobwoord@gmail.com>
2021-10-11 17:00:35 +0200

    refactored code

--- script.js
+++ script.js
@@ -4,35 +4,21 @@ const numOfPeople = document.querySelector(".input-box-num-people")
 const tip = document.querySelector(".tip-amount")
 const total = document.querySelector(".tip-amount-total")
 const reset = document.querySelector(".reset")
+const percentages = [5,10,15,25,50]
+const alert = "Please fill in the number of people"
 
+const onClick = (percentages) =>{
+  
+tip.innerHTML = `${Math.round(((percentages / 100) * billAmount.value) / numOfPeople.value)}`;
+total.innerHTML = `${Math.round((billAmount.value / numOfPeople.value) + (((percentages /100) * billAmount.value) / numOfPeople.value))}`;
 
+}
 
-button[0].addEventListener('click', () => {
-        tip.innerHTML = `${((5 / 100) * billAmount.value) / numOfPeople.value}`;
-        total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((5 /100) * billAmount.value) / numOfPeople.value)}`
-    })
+button.forEach((button, index) =>{
+    button.addEventListener('click', () =>{
+    onClick(percentages[index])
 
-button[1].addEventListener('click', () => {
-    
-    tip.innerHTML = `${((10 / 100) * billAmount.value) / numOfPeople.value}`;
-    total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((10 / 100) * billAmount.value) / numOfPeople.value)}`
-})
-
-button[2].addEventListener('click', () => {
-    tip.innerHTML = `${((15 / 100) * billAmount.value) / numOfPeople.value}`;
-    total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((15 / 100) * billAmount.value) / numOfPeople.value)}`
-})
-
-button[3].addEventListener('click', () => {
-
-    tip.innerHTML = `${((25 / 100) * billAmount.value) / numOfPeople.value}`
-    total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((25 /100) * billAmount.value) / numOfPeople.value)}`
-})
-
-button[4].addEventListener('click', () => {
-    
-    tip.innerHTML = `${((50 / 100) * billAmount.value) / numOfPeople.value}`
-    total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((50 /100) * billAmount.value) / numOfPeople.value)}`
+ })
 })
 
 reset.addEventListener('click', () => {
@@ -42,3 +28,42 @@ reset.addEventListener('click', () => {
     tip.innerHTML = '00.00';
     total.innerHTML = '00.00';
 })
+
+
+
+
+// button[0].addEventListener('click', () => {
+//         tip.innerHTML = `${((5 / 100) * billAmount.value) / numOfPeople.value}`;
+//         total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((5 /100) * billAmount.value) / numOfPeople.value)}`
+//     })
+
+// button[1].addEventListener('click', () => {
+    
+//     tip.innerHTML = `${((10 / 100) * billAmount.value) / numOfPeople.value}`;
+//     total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((10 / 100) * billAmount.value) / numOfPeople.value)}`
+// })
+
+// button[2].addEventListener('click', () => {
+//     tip.innerHTML = `${((15 / 100) * billAmount.value) / numOfPeople.value}`;
+//     total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((15 / 100) * billAmount.value) / numOfPeople.value)}`
+// })
+
+// button[3].addEventListener('click', () => {
+
+//     tip.innerHTML = `${((25 / 100) * billAmount.value) / numOfPeople.value}`
+//     total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((25 /100) * billAmount.value) / numOfPeople.value)}`
+// })
+
+// button[4].addEventListener('click', () => {
+    
+//     tip.innerHTML = `${((50 / 100) * billAmount.value) / numOfPeople.value}`
+//     total.innerHTML = `${(billAmount.value / numOfPeople.value) + (((50 /100) * billAmount.value) / numOfPeople.value)}`
+// })
+
+// reset.addEventListener('click', () => {
+//     billAmount.value = '';
+//     custom.value = '';
+//     numOfPeople.value = '';
+//     tip.innerHTML = '00.00';
+//     total.innerHTML = '00.00';
+// })
